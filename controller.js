8c9df16e2e73fbb66265d5232f1a6888ba160a46
rizal690 <rizalhendri935@outlook.com>
2020-04-05 01:13:52 +0700

    berhasil menampilkan data berdasarkan id

--- controller.js
+++ controller.js
@@ -3,18 +3,32 @@
 var response = require('./res');
 var connection = require('./koneksi');
 
-exports.index = function(red,res){
-    response.ok("Aplikasi REST API ku berjalan!",res)
+exports.index = function(req,res){
+    response.ok("Aplikasi REST API ku berjalan!", res)
 };
 
 //menampilkan semua data mahasiswa
-exports.tampilsemuamahasiswa = function(req,res){
-    connection.query('SELECT * FROM mahasiswa', function(error, rows, fileds){
+exports.tampilsemuamahasiswa = function(req, res){
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields){
         if(error){
-            connection.log(error);
+            console.log(error);
 
-        }else {
+        } else {
             response.ok(rows, res)
         }
     });
 };
+
+//menampilkan semua data mahasiswa berdasarkan id
+exports.tampilberdasarkanid = function (req, res) {
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
+        function(error, rows, fields) {
+            if(error) {
+                console.log(error);
+            } else {
+                response.ok(rows, res)
+            }
+        });
+};
+
